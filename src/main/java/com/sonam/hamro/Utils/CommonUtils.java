4f66948871d7f03945209812c8f2d39072b22b8b
Sonam Shrestha <sonamshrestha@nchl.local>
2022-12-25 11:17:46 +0545

    New Changes

--- src/main/java/com/sonam/hamro/Utils/CommonUtils.java
+++ src/main/java/com/sonam/hamro/Utils/CommonUtils.java
@@ -6,7 +6,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Objects;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class CommonUtils {
 
@@ -22,4 +23,40 @@ public class CommonUtils {
         Files.write(path1, fb);
         return "images/" + path + "/" + fileName;
     }
+    public static List<String> generateRandom(String name) {
+        int a = 1;
+        List<String> join = new ArrayList<>();
+        List<String> namesArray = Arrays.stream(name.split(",")).map(String::trim).collect(Collectors.toList());
+        List<String> names = namesArray.stream().map(String::trim).toList();
+
+        //Collections.shuffle(names);
+        Collections.shuffle(namesArray);
+
+        for (int i = 0; i < namesArray.size(); i++) {
+            if (namesArray.get(i).equals(names.get(i))) {
+                String nna = namesArray.remove(i);
+                namesArray.add(nna);
+            }
+        }
+        for (int i = 0; i < namesArray.size(); i++) {
+            if (names.get(i).equals(namesArray.get(i))) {
+                generateRandom(name);
+                break;
+            }
+            String giftNo = "";
+            if(names.get(i).contains("-")){
+                a = (i+1);
+                giftNo = a+"";
+            }else{
+                a++;
+                giftNo = a+"";
+            }
+            String giver = names.get(i).split("-").length > 1 ? names.get(i).split("-")[1]: names.get(i);
+            String receiver = namesArray.get(i).split("-").length > 1 ? namesArray.get(i).split("-")[1]: namesArray.get(i);
+            join.add("" + (giftNo) + ", " + giver + " -> " +receiver);
+        }
+        return join;
+
+    }
+
 }
--- src/main/java/com/sonam/hamro/config/WebSocketConfig.java
+++ src/main/java/com/sonam/hamro/config/WebSocketConfig.java
@@ -3,7 +3,6 @@ package com.sonam.hamro.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
